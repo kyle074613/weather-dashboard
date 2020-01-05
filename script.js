0d805e52eef8883d0bfbc7adeffbd6fa17ebbcdd
Kyle Jones <kyle074613@gmail.com>
2020-01-04 22:39:12 -0800

    script still needa more work

--- script.js
+++ script.js
@@ -4,16 +4,34 @@ var searchHistory = $("#search-history");
 var weatherCol = $("#weather-col");
 
 var apiKey = "4108a6c32359fff2574ba233a1ecfc25";
+var currentWeatherUrl;
+var forecastUrl;
 
 
-searchButton.on("click", function () {
-
-    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchBar.val() + "&units=imperial&appid=" + apiKey;
-
+function populateCurrentWeather() {
     $.ajax({
-        url: queryUrl,
+        url: currentWeatherUrl,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-    })
+
+    });
+}
+
+function populateWeatherForecast() {
+    $.ajax({
+        url: forecastUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+}
+
+searchButton.on("click", function () {
+
+    currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchBar.val() + "&units=imperial&appid=" + apiKey;
+    forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchBar.val() + "&units=imperial&appid=" + apiKey;
+
+    populateCurrentWeather();
+    populateWeatherForecast();
 });
