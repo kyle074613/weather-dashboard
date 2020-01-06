66b2ab3e9ab89a3aa8776d04bb9ab22b9d70b810
kyle074613 <kylemj@uci.edu>
2020-01-05 18:45:31 -0800

    current weather is displaying as intended, need to work on 5 day forecast

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ var weatherCol = $("#weather-col");
 var apiKey = "4108a6c32359fff2574ba233a1ecfc25";
 var currentWeatherUrl;
 var forecastUrl;
-var currentWeatherObj;
 var fiveDayForecastArray = [];
 
 //Creates current date variable
@@ -22,12 +21,15 @@ function populateCurrentWeather() {
         console.log(response);
 
         //Object to store current weather data
-        currentWeatherObj = {
+        var currentWeatherObj = {
+            location: response.name,
             date: currentDate,
             weatherIcon: response.weather[0].icon,
             temperature: Math.round(response.main.temp),
+            humidity: response.main.humidity,
             wind: response.wind.speed,
-            uvIndex: 0
+            uvIndex: 0,
+            uvIntensity: ""
         };
 
         //Call to get UV index 
@@ -41,9 +43,28 @@ function populateCurrentWeather() {
         }).then(function (response2) {
             console.log(response2);
             currentWeatherObj.uvIndex = response2.value;
+
+            //Assigns uvIntensity based on the uvIndex number (will be used for CSS styling)
+            if (currentWeatherObj.uvIndex >= 8)
+                currentWeatherObj.uvIntensity = "high";
+            else if (currentWeatherObj.uvIndex < 3)
+                currentWeatherObj.uvIntensity = "low";
+            else
+                currentWeatherObj.uvIntensity = "medium";
+
+            console.log(currentWeatherObj);
+
+            //Generates a card with all current weather info and appends it to the weather-col element
+            var currentWeatherCard = $('<div class="card"><div class="card-body"><h5 class="card-title">' + currentWeatherObj.location + ' (' + currentWeatherObj.date + ') ' +
+                '<img id="weather-icon" src="http://openweathermap.org/img/wn/' + currentWeatherObj.weatherIcon + '@2x.png"></h5>' +
+                '<p class="card-text">Temperature: ' + currentWeatherObj.temperature + ' °F</p>' +
+                '<p class="card-text">Humidity: ' + currentWeatherObj.humidity + '%</p>' +
+                '<p class="card-text">Wind Speed: ' + currentWeatherObj.wind + ' MPH</p>' +
+                '<p class="card-text">UV Index: <span class="badge badge-secondary ' + currentWeatherObj.uvIntensity + '">' + currentWeatherObj.uvIndex + '</span>')
+            $("#weather-col").append(currentWeatherCard);
         });
 
-        console.log(currentWeatherObj);
+
     });
 }
 
